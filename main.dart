81170c8449ede5c358f93697b624b2b7271ada61
ubaidshah708 <155091134+ubaidshah708@users.noreply.github.com>
2023-12-31 22:42:53 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,155 @@
+// qno 1
+void main(){
+int length = 12;
+int width = 15;
+if(length==width){
+  print("values are square");
+}else{
+  print("values are rectangle");
+}
+}
+
+// // // qno2
+
+void main()
+{
+  int age1 = 30;
+  int age2 = 31;
+  if(age1>age2){
+    print("$age1 is older than $age2");
+    } else {
+      print("$age1 is elder than $age2");
+  }
+}
+
+// // // q no 3
+
+
+// // void main() {
+// // int totClass = 16;
+// //  int attendence =10;
+// // double persentage = attendence*100 / totClass;
+
+// // if(persentage>= 75){
+// //   print("egilibe");}
+// //   else{
+// //     print("not egilible");
+
+// //   }
+// //   }
+// // qno5
+// // void main(){
+// // int temp = 42;
+// // if(temp<0){
+// // print("freezing weather");
+// // }
+// // else if(temp<=10){
+// //   print('very cold ');
+// // }
+// // else if(temp<=20){
+// //   print("thand ha");
+// // }
+// // else if (temp<=35){
+// //   print("axha mosam ha");
+// // }
+// // else{
+// //   print("mosam gram ha");
+// // }
+// qn08 
+// void main(){
+// int math = 85;
+// int eng = 99;
+// int isl = 100;
+// int bio = 80;
+// int phy = 90;
+// int obtmarks = math+eng+isl+bio+phy;
+// int totalmarks =500;
+// double persentage = obtmarks*100/totalmarks;
+// if(persentage>=80){
+// print(" name ali");
+// print("roll num 90800");
+// print("class 10");
+// print("grade A+");
+// }
+// else if(persentage<=70){
+//   print("GRADE A");
+// }
+// else if(persentage>=60){
+// print("grade C");
+// }
+// else{
+//   print("not allow to sit");
+// }
+
+// }
+// q no 10
+// void main(){
+// int user1 = 70;
+// int user2 = 60;
+// int user3 = 40;
+// if(user1>user2){
+// print("the greatesr num is$user1");
+// }else if(user2<user1){
+//   print("the middle num is $user2");
+// }
+// if(user2>user3){
+// print("the lowest num is $user3");
+// }
+// if(user3<user1){
+//   print("$user3 lowest num");
+// }
+// else{
+//   print ("sab galat ha");
+//  }}
+// qno4
+// void main (){
+// int celsius = 10;
+// double fahrenheit =(celsius*9/5)+32;
+// print("celsious in fahrenheit =$fahrenheit");
+// }
+// // qn06
+// void main(){
+// String alp1= "A E I O U";
+// String alp2 = " B C D F G H J K";
+// print("$alp1 is vowels");
+// print("$alp2 is consonant");}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
