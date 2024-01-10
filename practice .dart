e37a9e93db90edf9939f58ef74a7b701781b9712
ubaidshah708 <155091134+ubaidshah708@users.noreply.github.com>
2024-01-10 11:02:13 +0500

    Add files via upload

---
+++ practice .dart
@@ -0,0 +1,174 @@
+// void main(){
+// int num = 14;
+// if(num%2 == 0){
+// print("$num is even");
+// if(num%5 == 0){
+
+//   print("$num is divsble by 5");
+// } else{
+// print("num is not disble by 5");
+
+// }
+// } else{ 
+//   print(" num is odd");
+// }
+// }
+// void main() {
+// int num = 36;
+// if(num%2 == 0 ){
+// print("num is even");
+// if(num%6==0){
+//   print("num divisble 9");
+// }else{
+// print("$num is not divisabl 9");
+
+// }}else{
+// print(" num is not even");
+
+
+// }
+
+// }
+
+
+
+
+
+// void  main(){
+// int num = 99;
+// if(num%2 == 0){
+// print("num is even");
+// if(num%5==0){
+// print("num is divisible by 5");
+
+// }else{
+//   print("num is not divisible by 5");
+// }
+// }else{
+// print("num is odd");if(num%11==0){
+//   print("num is divisable by 11");
+// }
+
+// }
+// }
+// void main(){
+//   int num1 = 65;
+//   int num2 = 75;
+//   if(num1==num2){
+//     print("num1 is eqaul to num2");
+//   }else{
+//     print("num is not equal");
+//   }
+// }
+// void main(){
+// int age1 = 35;
+// int age2 = 95;
+// if(age1>age2){
+// print("$age1 is greater");
+
+// }else{
+//   print("$age2 is greater ");
+// }
+
+
+
+
+// }
+// import 'dart:math';
+// void main(){
+// double resault = sqrt(25);
+// print("sqare root of 25 is$resault");
+
+// }
+// void main(){
+//   List studentname =["ubaid","uzair","saad",1,2,3,4,5];
+// print(studentname);}
+
+// void main(){
+// List<String>studentname =["saad","anas",'ali',"zaid"];
+// print(studentname[1]);
+// }
+
+// void main(){
+//  List<String>studentname =["saad","anas",'ali',"zaid"];
+// print(studentname.elementAt(1));
+// }
+
+// void main (){
+// int num = 21;
+// if(num%5==0||num%7==0) {
+// print(" number is divi by5 or 7");
+// }else{
+//   print ("num is no divi by 5 or 7");
+// }}
+
+// void main (){
+// int num = 21;
+// if(num%5==0||num%7==0){
+// if(num%5==0){
+//   print("num is divi by ");
+// }else if(num%7==0){
+//   print("num is divin by 7");
+// }else{
+//   print("number is divi by both ");
+// }
+
+// }else {
+//   print("num is no divi by both");
+// }
+
+// }
+
+
+// void main(){
+// List <int>name= [1,2,3,4,5];
+// name[1]=9;
+// print(name);
+// }
+// void main(){
+// List <int>name= [1,2,3,4,5];
+// name.replaceRange(1,3, [2]);
+// print (name);
+
+
+// }
+void main(){
+List <int>name= [1,2,3,4,5];
+name.replaceRange(name.length -1,name.length, [2]);
+print (name);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
