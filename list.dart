2f6a34f84b2441553b5f818a7daf6ffd781e81d7
ubaidshah708 <155091134+ubaidshah708@users.noreply.github.com>
2024-01-11 10:36:57 +0500

    Add files via upload

---
+++ list.dart
@@ -0,0 +1,57 @@
+// void main(){
+// List name= ["ubaid","uzair","ali","wajid"];
+// name.replaceRange(1, 4,["saad","anas"]);
+// print(name);
+// void main(){
+// List num = [1,2,'ali','waqas'];
+// num[2]= 3;
+// print(num);
+// }
+// void main(){
+// List name = ["bilal ","zaheer","ali","wahab","ubaid"]; 
+// name.sort();
+// print(name);
+// }
+// void main(){
+// List num = [9,7,5,99,08,76,3,1,2,45,];
+// num.sort();
+// print(num); 
+// }
+
+// void main() {
+// var lst = [1, 2, 3, 4, 5];
+// var e1 = lst.first;
+// var e2 = lst.last;
+// var e3 = lst.elementAt(1);
+// var len = lst.length;
+// print('There are $len elements in the list');
+// print('The first element is $e1');
+// print('The last element is $e2');
+// print('The second element is $e3');
+// }
+// void main (){
+// List num = [1,2,3,4,5];
+// print(num.first);
+// print(num.last);
+// print(num.elementAt(3));
+// print(num.length);
+// }
+void main(){
+List name = [1,2,3,4];
+print(name.isEmpty);
+name.clear();
+print(name .isNotEmpty);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
