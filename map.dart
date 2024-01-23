28c9235b808b954d72426625593b920853860d92
ubaidshah708 <155091134+ubaidshah708@users.noreply.github.com>
2024-01-23 19:14:05 +0500

    Add files via upload

---
+++ map.dart
@@ -0,0 +1,88 @@
+// void main(){
+// Map name = { "1":"ubaid",2:"karachi"};
+// print (name);
+// print (name .values);
+// print (name.keys);
+
+// }
+// Map Size
+// void main()
+// {
+// Map fruit = {1: 'Apple', 2: "Orange"};
+// print(fruit.length);
+// print('There are ${fruit.length} elements in the map');
+// }
+// isEmpty & isNotEmpty Method
+// void main() {
+// var words = {
+// 1: 'sky',
+// 2: 'fly',
+// 3: 'ribbon',
+// 4: 'falcon',
+// 5: 'rock',
+// };
+// print(words.isEmpty);
+// print(words.isNotEmpty);
+// print('---------------');
+// words.clear();
+// print(words.isEmpty);
+// print(words.isNotEmpty);
+// }
+//  void main(){
+// Map zaq = {'a':'ali','b':'zaed','c':'hafiz'};
+// print(zaq.isEmpty);
+//  }
+// void main(){
+// Map zaq = {'a':'ali','b':'zaed','c':'hafiz'};
+// print(zaq.isNotEmpty);
+//  }
+// void main(){
+// Map zaq = {'a':'ali','b':'zaed','c':'hafiz'};
+// zaq.clear();
+// print(zaq.isNotEmpty);}
+// void main(){
+// Map xyz = {1:"umer","home":"baldia","job":"xyz",};
+// print("map:${xyz}");
+// xyz.addAll({"dept":"tom","email":"xyz@gmail"});
+// print("map after adding key & value$xyz");
+// }
+// void main() {
+// Map student = {'name':'Tom','age': 23};
+// print('Map :${student}');
+// student.addAll({'dept':'Civil','email':'tom@xyz.com'});
+// print('Map after adding key-values :${student}');}
+
+// void main (){
+// Map<String,dynamic >stdntinfo = {"name":"ali","age":"14"};
+// stdntinfo["class"] = 6;
+// print(stdntinfo);
+// }
+// void main() {
+// var fruit = {1: 'Apple', 2: 'Orange'};
+// fruit[3] = 'Banana';
+// print(fruit);
+// var val = fruit.putIfAbsent(3, () => 'Mango');
+// print(fruit);
+// print(val);
+// var val2 = fruit.putIfAbsent(4, () => 'Cherry');
+// print(fruit);
+// print(val2);
+// }
+// void main(){
+// Map fruit ={1:"banana",2:"apple"}; 
+// fruit.putIfAbsent(3, () => "mango");
+// print(fruit);
+
+
+
+
+
+
+
+
+
+
+}
+
+
+      
