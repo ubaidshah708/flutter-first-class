760450cf19e087cb4f210bb2959ceba1c3d5b0c3
ubaidshah708 <155091134+ubaidshah708@users.noreply.github.com>
2024-01-31 14:46:21 +0500

    Add files via upload

--- class .dart
+++ class .dart
@@ -28,6 +28,38 @@
 // var xyz = num.first;
 // print("$xyz is smallest value and $abc is greatest value");
 // }
+// qno5';
+// void main() {
+// Map  name= {"ali":03123456677,"ubaid":03456785434,"saqib":0354177556,"atif":0325436773
+// };
+// print(name.where((e)=>name.length==4).list());
+
+
+// void main() {
+//   Map<String, dynamic> person = {
+//     "name": "ali",
+//     "phone": "03456789023",
+//     "city": "karachi",
+//     "email": "ali@gmail.com",
+//   };
+
+//   var keysWithLength4 = person.keys.where((key) => key.length == 4);
+
+//   print("Keys with length 4: $keysWithLength4");
+// }
+
+
+void main() {
+ Map <dynamic,dynamic> lc_name ={"123345667" : "Taha",
+                                "1234" : "Ghazanfar",
+                                "232321" : "Mustafa",
+                                "3421" : "Asad Bhai"};
+ print(lc_name.keys.where((e) => e.length == 4)); 
+}
+
+
+ 
+
 // qno6
 // void main(){
 // Map world={"pakistan":{"city":"islamabd","language":"urdu","cruncy":"rupees"},
@@ -80,18 +112,119 @@
 // print (name.toSet());
 // }
 
-void main() {
-  var originalList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  int n = 3;
+//qn0 11 
+// void main() {
+//   var originalList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//   int n = 3;
+// var  result=originalList.sublist(0,n);
 
-  var result =originalList.insert;
-  print("The modified list is ${result}");
+//   print("Original List: $originalList");
+//   print("New List with first $n elements: $result");
+// }
 
-  print("Original List: $originalList");
-  print("New List with first $n elements: $result");
-}
+// qno12
+
+// void main (){
+// List name = ["ali","ubaid","owais","wajid","shahid"];
+// List abc = name.reversed.toList();
+// print("the originalListis$name");
+// print("the  reversed list is ${abc}");
+
+// }
+
+// qno13
+
+// void main() {
+//   List<int> originalList = [1, 2, 3, 2, 4, 5, 6, 1];
+//   List<int> resultList =L;
+//   print(resultList);
+// }
 
 
 
 
-  
+// void main(){
+// var  num1= [10,34,45,6,7,8,9,];
+// print("the original list  is ${num1}"p);
+// num1.sort();
+// print("The sorted list in ascending order is ${num1}");
+
+
+// void main(){
+// List num =[-5,-4,-3,-2,-1,0,1,2,3,4];
+// print("The given list of numbers is :${num}");
+// num.removeWhere((a)=>a< 0);
+// print("positivenumbers is${num}");
+// }
+// 
+// void main (){
+// List<dynamic> num = [1,2,3,4,5,6,7,8,9];
+// print ("Given list is ${num}");
+// print("after removing odd numbers${num.where(  (ele) => ele % 2 == 0)}");}
+
+
+// void main(){
+// List originalList = [1,2,3,4,5,6,7,8];
+// List result = originalList.map((e) => e*e).toList();
+// print(result);
+// }
+
+// qno18
+// void main() {
+//   Map<String, dynamic> person = {
+//     "name": "John",
+//     "age": 25,
+//     "isStudent": true,
+//   };
+//   if (person["isStudent"] &&person ["age"]>18) {
+//     print("Eligible");
+//   } else {
+//     print("Not eligible");
+//   }
+// }
+// qno19
+// void main (){
+// Map produce = {"name":"soap","price":"120","quantity":15};
+//   if(produce["quantity"]>0){
+// print( "in stock");
+//   } else{
+//     print("out of stock");
+//   }
+// }
+// qno20
+// void main(){
+// Map car = {
+// "name":"toyota",
+// "colour":"red" ,
+// "inseden":true,
+// };
+
+// if(car["colour"]=="red"&&car["inseden"]=="inseden"){
+// print ("match");
+
+// }else{
+//   print("not match");
+// }
+// }
+// qno12
+// void main() {
+//   Map<String, dynamic> user = {
+//     "name": "saqid",
+//     "isAdmin": true,
+//     "isActive": true,
+//   };
+//   if (user["isAdmin"] == true && user["isActive"] == true) {
+//     print("Active admin");
+//   } else {
+//     print("Not an active admin");
+//   }
+// }
+
+
+
+
+
+
+
+
+
